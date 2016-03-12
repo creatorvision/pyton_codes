a6943fc46758d2e022ab159feaa29cf96a5a3567
creatorvision <creatorvision03@gmail.com>
2016-03-12 22:12:11 +0530

    adding guessnumbergame python code

---
+++ GuessNumberGame.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import random
+
+var = random.randint(1,100000)
+
+while 1:
+
+	test= input('Enter a Integer Number of your wish : ')
+	if var == test:
+		print( "You've guessed the correct number")
+		break
+	elif var < test:
+		print( "Your guess is greater then the number")
+	else :
+		print(" Your guess is less than the number ")
+
